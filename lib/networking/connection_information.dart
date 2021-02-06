e501c79e7e5db57782d1fd6c54ffbc66615962e7
ChristianNomena <christiangrzk@gmail.com>
2021-02-06 19:29:40 +0300

    Server URL target changed

--- lib/networking/connection_information.dart
+++ lib/networking/connection_information.dart
@@ -1,4 +1,4 @@
 class ConnectionInformation {
   // static String url = "http://10.0.2.2/challenge/index.php";
-  static String url = "https://challenge-yourself-app.herokuapp.com/";
+  static String url = "https://challenge-yourself-app.herokuapp.com/server_operation.php";
 }
